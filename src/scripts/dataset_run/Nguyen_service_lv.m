286829fe56b7b217f724523f2ecc6154ed943afe
Yu Runfeng <85431219+yu-runfeng@users.noreply.github.com>
2025-06-23 14:21:12 +0800

    Add result existence check and refactor file handling
    Introduces a check to skip processing if both log and mat result files already exist, preventing redundant computation. Refactors variable names for clarity, consolidates result path usage, and removes unused count logic.

--- src/scripts/dataset_run/Nguyen_service_lv.m
+++ src/scripts/dataset_run/Nguyen_service_lv.m
@@ -8,36 +8,45 @@ clear;
 close all;
 
 data_path = './data/nguyen/';
-log_path = './result/matlab/nguyen/temp/';
-file_list = dir([data_path, '*.txt']);
-file_list = natsortfiles({file_list.name})';
+data_list = dir([data_path, '*.txt']);
+data_list = natsortfiles({data_list.name})';
 
-count = 0;
-for i = 1:length(file_list)
-    file_name = file_list{i};
+result_path = './result/matlab/nguyen/temp/';
+result_mat = dir([result_path, '*.mat']);
+result_mat = {result_mat.name}';
+result_log = dir([result_path, '*.txt']);
+result_log = {result_log.name}';
+
+for i = 1:length(data_list)
+    file_name = data_list{i};
     data = Read_Nguyen([data_path, file_name]);
     [param_lr, param_ts, param_lr_gts] = Param_Setting(data);
 
-    if count < 8
-        count = count + 1;
-        continue
-    end
-
     for srv_lv = [0.75, 0.8, 0.85, 0.9, 0.95]
         data.srv_lv = srv_lv;
+
         for run_time = 1:10
             rng(run_time);
+
             disp([file_name(1:end - 4), '-r-', num2str(run_time)]);
             log_name = [file_name(1:end - 4), ...
                 '-r-', num2str(run_time), ...
                 '-srv-', num2str(srv_lv), '.txt'];
             disp(log_name);
 
+            id_name = log_name(1:end - 4);
+
+            
+            % if log file and mat file both exist, skip it
+            if Is_Exist(id_name, result_log, result_mat)
+                continue
+            end
+
             try
-                diary([log_path, log_name]);
+                diary([result_path, log_name]);
             catch
-                mkdir(log_path);
-                diary([log_path, log_name]);
+                mkdir(result_path);
+                diary([result_path, log_name]);
             end
 
             solution = Lr_Gts_mex(data, param_lr_gts, param_lr, param_ts);
@@ -46,13 +55,34 @@ for i = 1:length(file_list)
             mat_name = ...
                 [log_name(1:end - 4), '-', num2str(solution.value), '.mat'];
             try
-                save([log_path, mat_name], 'solution');
+                save([result_path, mat_name], 'solution');
             catch
-                mkdir(log_path);
-                save([log_path, mat_name], 'solution');
+                mkdir(result_path);
+                save([result_path, mat_name], 'solution');
             end
+
             diary off;
         end
     end
-    count = count + 1;
+end
+
+function is_exist = Is_Exist(id_name, result_log, result_mat)
+
+log_flag = false;
+for i = 1:length(result_log)
+    if contains(result_log{i}, id_name)
+        log_flag = true;
+        break;
+    end
+end
+
+mat_flag = false;
+for i = 1:length(result_mat)
+    if contains(result_mat{i}, id_name)
+        mat_flag = true;
+        break;
+    end
+end
+
+is_exist = log_flag & mat_flag;
 end
