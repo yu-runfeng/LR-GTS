75f4375dadc0fdc1dbc3b34e88069748b9566a50
Yu Runfeng <85431219+yu-runfeng@users.noreply.github.com>
2025-07-30 00:23:02 +0800

    add coder project

--- src/coder_entry/Coder_Lr_Gts.m
+++ src/coder_entry/Coder_Lr_Gts.m
@@ -4,14 +4,9 @@
 clc;
 clear all;
 
-data = Read_Nguyen('../../data/nguyen/25-5MNb.txt');
+data = Read_Snyder('../../data/snyder/15nodes/');
 
-[param_lr, param_ts, param_lr_gts] = Param_Setting(data);
-
+param_lr = Param_Setting();
 param_lr.ITER_MAX = 5; % maximum LR iteration
-param_ts.ITER_MAX = 10;
-param_ts.DIV_ITER_UNIMP = 3;
-param_lr_gts.ITER_UNIMP = 3; % uninproved interation
-param_lr_gts.TIME_LIMIT = 10; % time limit
 
-solution = Lr_Gts(data, param_lr_gts, param_lr, param_ts);
+best_sol = Lagrangian_Relaxation(data, param_lr);
