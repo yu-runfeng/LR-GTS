277fcccfa045ef1003ef2efd52828d1593784fff
Yu Runfeng <85431219+yu-runfeng@users.noreply.github.com>
2025-07-29 22:13:25 +0800

    Update Coder_Lr_Gts.m

--- src/coder_entry/Coder_Lr_Gts.m
+++ src/coder_entry/Coder_Lr_Gts.m
@@ -4,14 +4,17 @@
 clc;
 clear all;
 
-data = Read_Nguyen('./data/nguyen/25-5MNb.txt');
+file_path = './data/tuzun/coordP111112.dat';
+data = Read_Tuzun(file_path);
 
 [param_lr, param_ts, param_lr_gts] = Param_Setting(data);
 
-param_lr.ITER_MAX = 5; % maximum LR iteration
+param_lr.ITER_MAX = 20; % maximum LR iteration
+
 param_ts.ITER_MAX = 10;
 param_ts.DIV_ITER_UNIMP = 3;
-param_lr_gts.ITER_UNIMP = 3; % uninproved interation
-param_lr_gts.TIME_LIMIT = 10; % time limit
+
+param_lr_gts.ITER_UNIMP = 10; % uninproved interation
+param_lr_gts.TIME_LIMIT = 120; % time limit
 
 solution = Lr_Gts(data, param_lr_gts, param_lr, param_ts);
